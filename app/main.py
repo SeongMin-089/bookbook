15bb4387ef28a801dd7a71c0db2fef8750223f4c
unknown <a99701841@gmail.com>
2026-05-26 19:28:58 +0900

    mongoDB update

--- app/main.py
+++ app/main.py
@@ -1,23 +1,39 @@
-from pathlib import Path
-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+BASE_DIR=Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=BASE_DIR/ "templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/",response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse(request, "index.html", {"title": "북북"})
-
-
-@app.get("/search", response_class=HTMLResponse)
-async def search(request: Request, q: str):
     return templates.TemplateResponse(
-        request, "index.html", {"title": "북북", "keyword": q}
+        request,
+        "index.html",
+        {"title":"북북"}
+    ) 
+
+
+@app.get("/search",response_class=HTMLResponse)
+async def read_item(request:Request,q:str):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {"keyword":q}
     )
+
+
+@app.on_event("startup")
+async def on_app_start():
+    print("hello server")
+
+@app.on_event("shutdown")
+async def on_app_shutdown():
+    print("goodbye server")

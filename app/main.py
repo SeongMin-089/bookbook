914a701dcf2d3c4731dce189b6b0205e40887700
unknown <a99701841@gmail.com>
2026-05-26 18:23:24 +0900

    venv 파이썬 버전 변경

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
+from pathlib import Path
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
 
-BASE_DIR=Path(__file__).resolve().parent
-templates = Jinja2Templates(directory=BASE_DIR/ "templates")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-@app.get("/",response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"title":"북북"}
-    )
+    return templates.TemplateResponse(request, "index.html", {"title": "북북"})
 
 
-@app.get("/search",response_class=HTMLResponse)
-async def read_item(request:Request, q:str):
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
     return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"keyword":q}
+        request, "index.html", {"title": "북북", "keyword": q}
     )

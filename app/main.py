bdc878fae02df2fd51694350df4c0a51d53f61c6
unknown <a99701841@gmail.com>
2026-05-28 17:17:19 +0900

    0528 검색 처리 완

--- app/main.py
+++ app/main.py
@@ -4,6 +4,12 @@ from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from pathlib import Path
 
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from app.models import mongodb
+from app.models.book import BookModel
+from app.book_scraper import NaverBookScraper
+
 
 app = FastAPI()
 
@@ -14,6 +20,14 @@ templates = Jinja2Templates(directory=BASE_DIR/ "templates")
 
 @app.get("/",response_class=HTMLResponse)
 async def root(request: Request):
+    # book=BookModel(
+    #     keyword="python",
+    #     publisher="lizb",
+    #     price=1200,
+    #     image="me.png"
+    # )
+    # save_book= await mongodb.engine.save(book)
+    # print(save_book.model_dump(),flush=True)
     return templates.TemplateResponse(
         request,
         "index.html",
@@ -23,17 +37,45 @@ async def root(request: Request):
 
 @app.get("/search",response_class=HTMLResponse)
 async def read_item(request:Request,q:str):
+    keyword=q
+
+    if not keyword:
+        return templates.TemplateResponse(
+            request=request,
+            name="index.html",
+            context={"message":"검색어를 입력해주세요"}
+        )
+    naver_book_scraper=NaverBookScraper()
+
+    books = await naver_book_scraper.search(keyword,10)
+
+    book_models=[]
+
+    for book in books:
+        print(book)
+        book_model=BookModel(
+            keyword=keyword,
+            publisher=book["publisher"],
+            price=int(book.get("discount") or 0),
+            image=book["image"]
+        )
+        book_models.append(book_model)
+
+    await mongodb.engine.save_all(book_models)   
+
     return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"keyword":q}
+        request=request,
+        name="index.html",
+        context= {"keyword":q,"books":book_models}
     )
 
 
 @app.on_event("startup")
 async def on_app_start():
     print("hello server")
+    mongodb.connect()
 
 @app.on_event("shutdown")
 async def on_app_shutdown():
     print("goodbye server")
+    mongodb.close()
